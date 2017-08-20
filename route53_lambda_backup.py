21dcffedb741c877415c2f8cdb24cacef56f9f8b
Jacob F. Grant <git@jacobfgrant.com>
2017-08-20 02:28:44 -0700

    Create a simple Route 53 Lambda backup script
    Creates a basic Python 3 script to be run as an AWS Lambda function or
    cron job to back up Route 53 DNS records to an AWS S3 bucket. While
    functional, this script is somewhat simplistic and crude, and lacks
    meaningful documentation.
    
    Going forward, the code should be refactored to make it
    cleaner and more readable. Documentation and comments should also be
    added and expanded. Making the code backwards-compatible with Python 2.7
    should also be considered.
    
    Future development should also include a Python or shell script to
    upload this script and create the Lambda function, complete with IAM
    role and environmental variables.
    
    The backup csv files generated by this script can be restored using the
    route53-transfer module. The code and documentation for this module,
    including how to restore the Route 53 DNS records, can be found here:
    
    https://github.com/RisingOak/route53-transfer

---
+++ route53_lambda_backup.py
@@ -0,0 +1,208 @@
+"""AWS Route 53 Lambda Backup"""
+
+import os
+import csv
+import time
+from datetime import datetime
+import boto3
+
+
+# Set environmental variables
+
+s3_bucket_name = ''
+s3_bucket_region = ''
+
+try:
+    s3_bucket_name = os.environ['s3_bucket_name']
+    s3_bucket_region = os.environ['s3_bucket_region']
+except KeyError as e:
+    print("Warning: Environmental variable(s) not defined")
+
+
+# Create client objects
+
+s3 = boto3.client('s3', region_name='us-east-1')
+route53 = boto3.client('route53')
+
+
+# Functions
+
+def create_s3_bucket(bucket_name, bucket_region='us-east-1'):
+    """Create an Amazon S3 bucket."""
+    # creating bucket in N. Virginia requires no CreateBucketConfiguration parameter
+    if(bucket_region == 'us-east-1'):
+        try:
+            s3.create_bucket(
+                ACL='private',
+                Bucket=bucket_name
+            )
+        except Exception as e:
+            print(e)
+    else:
+        try:
+            s3.create_bucket(
+                ACL='private',
+                Bucket=bucket_name,
+                CreateBucketConfiguration={
+                    'LocationConstraint': bucket_region
+                }
+            )
+        except Exception as e:
+            print(e)
+    return
+
+
+def upload_to_s3(folder, filename, bucket_name, key):
+    """Upload a file to a folder in an Amazon S3 bucket."""
+    key = folder + '/' + key
+    s3.upload_file(filename, bucket_name, key)
+    return
+
+
+def get_route53_hosted_zones(next_zone=None):
+    """Recursively returns a list of hosted zones in Amazon Route 53."""
+    if(next_zone):
+        response = route53.list_hosted_zones_by_name(DNSName=next_zone[0], HostedZoneId=next_zone[1])
+    else:
+        response = route53.list_hosted_zones_by_name()
+    hosted_zones = response['HostedZones']
+    # if response is truncated, call function again with next zone name/id
+    if(response['IsTruncated']):
+        hosted_zones += get_route53_hosted_zones((response['NextDNSName'], response['NextHostedZoneId']))
+    return hosted_zones
+
+
+def get_route53_zone_records(zone_id, next_record=None):
+    """Recursively returns a list of records of a hosted zone in Amazon Route 53."""
+    if(next_record):
+        response = route53.list_resource_record_sets(HostedZoneId=zone_id, StartRecordName=next_record[0], StartRecordType=next_record[1])
+    else:
+        response = route53.list_resource_record_sets(HostedZoneId=zone_id)
+    zone_records = response['ResourceRecordSets']
+    # if response is truncated, call function again with next record name/id
+    if(response['IsTruncated']):
+        zone_records += get_route53_zone_records(zone_id, (response['NextRecordName'], response['NextRecordType']))
+    return zone_records
+
+
+def get_record_value(record):
+    """Return a list of values for a hosted zone record."""
+    # test if record's value is Alias or dict of records
+    try:
+        value = [':'.join(
+            ['ALIAS', record['AliasTarget']['HostedZoneId'],
+            record['AliasTarget']['DNSName']]
+        )]
+    except KeyError:
+        value = []
+        for v in record['ResourceRecords']:
+            value.append(v['Value'])
+    return value
+
+
+def try_record(test, record):
+    """Return a value for a record"""
+    # test for Key and Type errors
+    try:
+        value = record[test]
+    except KeyError:
+        value = ''
+    except TypeError:
+        value = ''
+    return value
+
+
+def write_zone_to_csv(zone):
+    """Write hosted zone records to a csv file in /tmp/."""
+    zone_file_name = '/tmp/' + zone['Name'] + 'csv'
+    zone_records = get_route53_zone_records(zone['Id'])
+    # write to csv file with zone name
+    with open(zone_file_name, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        # write column headers
+        writer.writerow([
+            'NAME', 'TYPE', 'VALUE',
+            'TTL', 'REGION', 'WEIGHT',
+            'SETID', 'FAILOVER', 'EVALUATE_HEALTH'
+            ])
+        # loop through all the records for a given zone
+        for record in zone_records:
+            csv_row = [''] * 9
+            csv_row[0] = record['Name']
+            csv_row[1] = record['Type']
+            csv_row[3] = try_record('TTL', record)
+            csv_row[4] = try_record('Region', record)
+            csv_row[5] = try_record('Weight', record)
+            csv_row[6] = try_record('SetIdentifier', record)
+            csv_row[7] = try_record('Failover', record)
+            csv_row[8] = try_record('EvaluateTargetHealth', try_record('AliasTarget', record))
+            value = get_record_value(record)
+            # if multiple values (e.g., MX records), write each as its own row
+            for v in value:
+                csv_row[2] = v
+                writer.writerow(csv_row)
+    return
+
+
+# this function is redundant !!!
+# one of these two identical functions needs to be removed
+def write_zone_to_csv(zone):
+    """Write hosted zone records to a csv file in /tmp/."""
+    zone_file_name = '/tmp/' + zone['Name'] + 'csv'
+    zone_records = get_route53_zone_records(zone['Id'])
+    with open(zone_file_name, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow([
+            'NAME', 'TYPE', 'VALUE',
+            'TTL', 'REGION', 'WEIGHT',
+            'SETID', 'FAILOVER', 'EVALUATE_HEALTH'
+        ])
+        # include record values to loop through
+        record_values = [
+            'Name', 'Type', 'Value',
+            'TTL', 'Region', 'Weight',
+            'SetIdentifier', 'Failover',
+            'EvaluateTargetHealth'
+        ]
+        # loop through all the records for a given zone
+        for record in zone_records:
+            value = get_record_value(record)
+            i = 0
+            csv_row = [''] * 9
+            while i < len(record_values):
+                # skip 'Value'
+                if(record_values[i] == 'Value'):
+                    i += 1
+                    continue
+                # skip 'EvaluateTargetHealth'
+                if(record_values[i] == 'EvaluateTargetHealth'):
+                    record = try_record('AliasTarget', record)
+                csv_row[i] = try_record(record_values[i], record)
+                i += 1
+            # if multiple values (e.g., MX records), write each as its own row
+            for v in value:
+                csv_row[2] = v
+                writer.writerow(csv_row)
+    return
+
+
+# Handler Function
+
+def lambda_handler(event, context):
+    """Handler function for AWS Lambda"""
+    time_stamp = time.strftime("%Y-%m-%dT%H:%M:%SZ", datetime.utcnow().utctimetuple())
+    create_s3_bucket(s3_bucket_name, s3_bucket_region)
+    hosted_zones = get_route53_hosted_zones()
+    for zone in hosted_zones:
+        write_zone_to_csv(zone)
+        upload_to_s3(
+            time_stamp,
+            ('/tmp/' + zone['Name'] + 'csv'),
+            s3_bucket_name,
+            (zone['Name'] + 'csv')
+        )
+    return
+
+
+if __name__ == "__main__":
+    lambda_handler(0, 0)
